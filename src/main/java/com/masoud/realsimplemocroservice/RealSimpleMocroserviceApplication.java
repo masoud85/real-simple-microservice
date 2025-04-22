a0a661d3bf11a0576902ea75b63526bbbe330a93
Masoud <masoud.ahadi@gmail.com>
2025-04-22 12:59:31 +0200

    initial commit

---
+++ src/main/java/com/masoud/realsimplemocroservice/RealSimpleMocroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.masoud.realsimplemocroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealSimpleMocroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealSimpleMocroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/masoud/realsimplemocroservice/RealSimpleMocroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masoud.realsimplemocroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealSimpleMocroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
